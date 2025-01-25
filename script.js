a07c534b7da8b70b7a85c058e58a2d88a0b6d35c
Kyoli48 <24942009-Kyoli48@users.noreply.replit.com>
2025-01-25 17:44:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+//Add your global variables here
+
+
+
+
+
+// Add your functions here
+
+
+
+
+
+
+
+
+
+
+// Sound Synthesis Functions for Steps 6-8
+// You do not need to edit the below code
+const freqMap = {
+  1: 261.6,
+  2: 329.6,
+  3: 392,
+  4: 466.2
+}
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  context.resume()
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    context.resume()
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    context.resume()
+    tonePlaying = true
+  }
+}
+function stopTone(){
+  g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+  tonePlaying = false
+}
+
+// Page Initialization
+// Init Sound Synthesizer
+let AudioContext = window.AudioContext || window.webkitAudioContext 
+let context = new AudioContext()
+let o = context.createOscillator()
+let g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
